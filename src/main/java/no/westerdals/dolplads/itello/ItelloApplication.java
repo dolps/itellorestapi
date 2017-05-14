0e8428632be0c146fbe200fa5bb50ce7063c8bfd
dolps <dolplads@gmail.com>
2017-05-14 23:10:10 +0200

    added to heroku

--- src/main/java/no/westerdals/dolplads/itello/ItelloApplication.java
+++ src/main/java/no/westerdals/dolplads/itello/ItelloApplication.java
@@ -3,6 +3,13 @@ package no.westerdals.dolplads.itello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * Application added at https://devcenter.heroku.com/articles/deploying-spring-boot-apps-to-heroku#preparing-a-spring-boot-app-for-heroku
+ * <p>
+ * open app from Terminal with the heroku open command
+ * see logs with the heroku logs --tail
+ */
 @SpringBootApplication
 public class ItelloApplication {
 
