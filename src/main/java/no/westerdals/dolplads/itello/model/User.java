474bff46b2445b06dff90fd4cc2bddb97091aa49
dolps <dolplads@gmail.com>
2017-05-14 21:33:15 +0200

    added all

--- src/main/java/no/westerdals/dolplads/itello/model/User.java
+++ src/main/java/no/westerdals/dolplads/itello/model/User.java
@@ -3,11 +3,14 @@ package no.westerdals.dolplads.itello.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.Entity;
+
 /**
  * Created by dolplads on 14/05/2017.
  */
 @AllArgsConstructor
 @Data
+@Entity
 public class User {
     private String firstName;
     private String lastName;
